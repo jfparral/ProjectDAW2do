e03c52594fdba062b3867b3c9b68f21c239c84a6
jealvia <jealvia@espol.edu.ec>
2018-07-26 20:28:36 -0500

    Se agrego el modelo libro y autor y la app Registro con el modelo Usuario

--- LeoBook/models.py
+++ LeoBook/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    nombre=models.CharField(max_length=50)
+    apellido1=models.CharField(max_length=50)
+    apellido2=models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+class Libro(models.Model):
+    nombre=models.CharField(max_length=50)
+    precio=models.IntegerField(max_length=30)
+    descripcion=models.CharField(max_length=250)
+    libro=models.ManyToManyField(Autor)
+    def __str__(self):
+        return self.nombre
