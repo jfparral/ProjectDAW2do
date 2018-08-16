8c93acb440a421ad32bf621d134065f7e2911e57
LeonCastroA <leancast@espol.edu.ec>
2018-08-15 21:24:59 -0500

    Adding web-loader

--- ProjectDAW2do/settings.py
+++ ProjectDAW2do/settings.py
@@ -51,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+WEBPACK_LOADER = {
+    'DEFAULT': {
+            'BUNDLE_DIR_NAME': 'bundles/',
+            'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
+        }
+}
 
 ROOT_URLCONF = 'ProjectDAW2do.urls'
 
