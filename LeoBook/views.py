df7e74a4a80c8c220388749a5f368fa8b2bb8908
LeonCastroA <leancast@espol.edu.ec>
2018-08-24 11:28:00 -0500

    creating csv file for d3.js

--- LeoBook/views.py
+++ LeoBook/views.py
@@ -2,6 +2,31 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def index(request):
+    cat=Categoria.objects.all()
+    lib=Libro.objects.all()
+    aut=Autor.objects.all()
+    d={}
+for elem in lib:
+    _aut=elem.id_autor
+    _cat=elem.id_categoria
+    if _aut not in d:
+        d[_aut]={_cat:[elem]}
+    else:
+        if _cat not in d[_aut]:
+            d[_aut][_cat]=[elem]
+        else:
+            d[_aut][_cat].append(elem)
+f=open("mycsv.csv","w")
+import random as rd
+f.write("id,value\n")
+for el in d:
+    f.write(el+",\n")
+    for elemento in d[el]:
+        f.write(el+"."+elemento+",\n")
+        for nin in d[el][elemento]:
+            f.write(el + "." + elemento+"."+nin + "," + str(rd.randint(100, 3500)) + ",\n")
+f.close()
+
     return render(request,'LeoBook/index.html')
 
 def inicio(request):
