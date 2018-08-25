27cefe9706806f2b63dd3f0b4319a6151fdab1d1
LeonCastroA <leancast@espol.edu.ec>
2018-08-25 05:44:30 -0500

    csv file created and changes in chart.html

--- LeoBook/views.py
+++ LeoBook/views.py
@@ -1,31 +1,29 @@
 from django.shortcuts import render
 from .models import *
+import random as rd
 # Create your views here.
 def index(request):
-    cat=Categoria.objects.all()
     lib=Libro.objects.all()
-    aut=Autor.objects.all()
     d={}
-for elem in lib:
-    _aut=elem.id_autor
-    _cat=elem.id_categoria
-    if _aut not in d:
-        d[_aut]={_cat:[elem]}
-    else:
-        if _cat not in d[_aut]:
-            d[_aut][_cat]=[elem]
+    for elem in lib:
+        _aut=elem.id_autor
+        _cat=elem.id_categoria
+        if _aut not in d:
+            d[_aut]={_cat:[elem]}
         else:
-            d[_aut][_cat].append(elem)
-f=open("mycsv.csv","w")
-import random as rd
-f.write("id,value\n")
-for el in d:
-    f.write(el+",\n")
-    for elemento in d[el]:
-        f.write(el+"."+elemento+",\n")
-        for nin in d[el][elemento]:
-            f.write(el + "." + elemento+"."+nin + "," + str(rd.randint(100, 3500)) + ",\n")
-f.close()
+            if _cat not in d[_aut]:
+                d[_aut][_cat]=[elem]
+            else:
+                d[_aut][_cat].append(elem)
+    f=open("mycsv.csv","w")
+    f.write("id,value\n")
+    for el in d:
+        f.write(el.nombre+",\n")
+        for elemento in d[el]:
+            f.write(el.nombre+"."+elemento.nombre+",\n")
+            for nin in d[el][elemento]:
+                f.write(el.nombre + "." + elemento.nombre+"."+nin.nombre + "," + str(rd.randint(100, 3500)) + ",\n")
+    f.close()
 
     return render(request,'LeoBook/index.html')
 
