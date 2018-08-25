ccb579cbfabe33000e2973372dd0cc2343916197
jfparral <jparrales15@gmail.com>
2018-08-25 16:24:28 -0500

    subiendo rutas del API

--- LeoBookAPI/urls.py
+++ LeoBookAPI/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path
+from django.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('inicio',views.inicio,name="inicio"),
-    path('blog',views.blog,name='blog'),
-    path('chart',views.chart,name='chart'),
-    path('nosotros',views.nosotros,name='nosotros'),
-    path('toplibros',views.toplibros,name='toplibros'),
-    path('unete',views.unete,name='unete')
+    url(r'^author/$', views.author_list),
+    url(r'^category/$', views.category_list),
+    url(r'^book/$', views.book_list),
+    url(r'^booksell/(?P<user>[0-9]+)/(?P<book>[0-9]+)/$', views.book_sell),
+    url(r'^user_reserve/(?P<user>[0-9]+)/(?P<book>[0-9]+)/$', views.book_reserve),
+    url(r'^user_buy/(?P<user>[0-9]+)/$', views.user_detail_compras),
+    url(r'^user_register/$', views.crear),
+    url(r'^user_update/(?P<user>[0-9]+)/$', views.user_update),
+    url(r'^user_delete/(?P<user>[0-9]+)/$', views.user_delete),
+    url(r'^blog/$', views.blog_list),
+    url(r'^blog_id/(?P<blog_id>[0-9]+)/$', views.blog_id),
+    url(r'^event/$', views.event_list),
+    url(r'^event_id/(?P<event_id>[0-9]+)/$', views.event_id),
 ]
