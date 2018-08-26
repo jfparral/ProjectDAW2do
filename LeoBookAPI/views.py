e8f3ab4dd15e00ccb2e0d8380289f5ab06906bf8
jfparral <jparrales15@gmail.com>
2018-08-26 13:51:07 -0500

    a pedido de Julio

--- LeoBookAPI/views.py
+++ LeoBookAPI/views.py
@@ -126,8 +126,8 @@ class user_detail_compras(APIView):
 
 class crear(APIView):
     def post(self, request):
-        datos={'nombres':request.POST['nombre'],'image' : request.POST['image'],'correo':request.POST['correo'],'password':request.POST['password']
-        ,'id_libro_fav':[request.POST['id_libro_fav']],'id_autor_fav':[request.POST['id_autor_fav']]}
+        datos={'nombres':request.POST['nombre'],'correo':request.POST['correo'],'password':request.POST['password']
+        ,'id_libro_fav':[request.POST['id_libro_fav']],'id_autor_fav':[request.POST['id_autor_fav']],'image' : request.FILES['image'].file}
         serializer = UsuarioSerializer(data=datos)
         if serializer.is_valid():
             serializer.save()
