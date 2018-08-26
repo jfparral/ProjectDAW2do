4b53de3497f7fdfbddf085a520ab7a63f9218e87
jfparral <jparrales15@gmail.com>
2018-08-26 17:29:50 -0500

    agregado paginas solo para el usuario

--- LeoBookConsumer/urls.py
+++ LeoBookConsumer/urls.py
@@ -4,13 +4,16 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('indexUser',views.indexUser,name='indexUser'),
     path('login',views.login,name="login"),
     path('authenticate',views.authenticate,name="authenticate"),
     path('register',views.register,name="register"),
     path('sendRegister',views.sendRegister,name="sendRegister"),
     path('blog',views.blog,name='blog'),
+    path('blogUser',views.blogUser,name='blogUser'),
     path('chart',views.chart,name='chart'),
     path('nosotros',views.nosotros,name='nosotros'),
+    path('nosotrosUser',views.nosotrosUser,name='nosotrosUser'),
     path('toplibros',views.toplibros,name='toplibros'),
     path('unete',views.unete,name='unete'),
     path('mycsv.csv',views.csv,name='mycsv.csv'),
--- LeoBookConsumer/views.py
+++ LeoBookConsumer/views.py
@@ -61,6 +61,57 @@ def index(request):
         return render(request, 'LeoBook/index.html', context)
 
 
+def indexUser(request):
+    if request.method == 'GET':
+        booklist= requests.get('http://127.0.0.1:8000/book/')
+        books = booklist.json()
+        import os
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, 'templates\\LeoBook\\mycsv.csv')
+        d={}
+        print(books)
+        for elem in books:
+            l1=[]
+            l2=[]
+            for aut in elem['id_autor']:
+                lis_aut=requests.get('http://127.0.0.1:8000/author/')
+                lis_aut=lis_aut.json()
+                for a in lis_aut:
+                    if a['id']==aut:
+                        l1.append(a['nombre'])        
+            for cc in elem['id_categoria']:
+                lis_cat=requests.get('http://127.0.0.1:8000/category/')
+                lis_cat=lis_cat.json()
+                for c in lis_cat:
+                    if c['id']==cc:
+                        l2.append(c['nombre']) 
+            _aut=",".join(l1)
+            _cat=",".join(l2)
+            if _aut not in d:
+                d[_aut]={_cat:[elem]}
+            else:
+                if _cat not in d[_aut]:
+                    d[_aut][_cat]=[elem]
+                else:
+                    d[_aut][_cat].append(elem)
+        f=open(filename,"w",encoding="utf-8")
+        f.write("id,value\n")
+        f.write("LeoBook,\n")
+        for el in d:
+            f.write("LeoBook|"+str(el)+",\n")
+            for elemento in d[el]:
+                f.write("LeoBook|"+str(el)+"|"+str(elemento)+",\n")
+                for nin in d[el][elemento]:
+                    f.write("LeoBook|"+str(el) + "|" + str(elemento)+"|"+str(nin['nombre']) + "," + str(rd.randint(100, 1500)) + ",\n")
+        f.close()
+        context = {
+            'nombre' : request.session['user_name'],
+            'id': request.session['user_id'],
+            'books' : books
+        }
+        return render(request, 'LeoBook/indexUser.html', context)
+
+
 def login(request):
     """ send_mail(
         'Subject here',
@@ -82,10 +133,14 @@ def authenticate(request):
         else:
             request.session['user_name'] = user['nombres']
             request.session['user_id'] = user['id']
+            booklist= requests.get('http://127.0.0.1:8000/book/')
+            books = booklist.json()
             context = {
-                'user' : user
+                'nombre' : request.session['user_name'],
+                'id': request.session['user_id'],
+                'books' : books
             }
-            response = render(request,'LeoBook/dashboard.html',context)
+            response = render(request,'LeoBook/indexUser.html',context)
             return response
             
 
@@ -115,6 +170,16 @@ def blog(request):
     }
     return render(request, 'LeoBook/blog.html',context)
 
+def blogUser(request):
+    blog_list = requests.get('http://127.0.0.1:8000/blog/')
+    blogs = blog_list.json()
+    context = {
+        'nombre' : request.session['user_name'],
+        'id': request.session['user_id'],
+        'blogs' : blogs
+    }
+    return render(request, 'LeoBook/blogUser.html',context)
+
 def csv(request):
     return render(request,'LeoBook/mycsv.csv')
 def chart(request):
@@ -135,6 +200,13 @@ def unete(request):
 
 def nosotros(request):
     return render(request, 'LeoBook/nosotros.html')
+
+def nosotrosUser(request):
+    context = {
+        'nombre' : request.session['user_name'],
+        'id': request.session['user_id'],
+    }
+    return render(request, 'LeoBook/nosotrosUser.html',context)
 
 
 def toplibros(request):
